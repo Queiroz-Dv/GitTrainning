52ca9c2735715c18474b13203143bac12d0f4484
Queiroz <kalifor_dbel@hotmail.com>
2023-06-06 17:55:05 -0300

    Add project files.

---
+++ GitTrainning/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GitTrainning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aqui faremos um exemplo simples de como utilizar o git e github em conjunto
+            // Como estou sem microfone basta acompanhar tudo o que for feito aqui <-
+            
+
+            // Antes de tudo, verifique se você tem o git e github instalado, caso não tenha instale-os
+            // As abas ao lado são das duas ferramentas que mais 
+            // utilizamos no dia a dia (dependendo da empresa, projeto e 
+            // grupo que seu código fonte estará guardado)
+
+            // Você pode criar um repositório local com o git
+            // Como também você pode ter um repositório remoto (que é o github)
+        }
+    }
+}
