94958bcfd26eec151b2a5ed5852cad2fa2bba318
Queiroz <kalifor_dbel@hotmail.com>
2023-06-08 08:45:32 -0300

    Removendo comentários desnecessários.

--- GitTrainning/Program.cs
+++ GitTrainning/Program.cs
@@ -6,17 +6,11 @@ namespace GitTrainning
     {
         static void Main(string[] args)
         {
-            // Aqui faremos um exemplo simples de como utilizar o git e github em conjunto
-            // Como estou sem microfone basta acompanhar tudo o que for feito aqui <-
-            
+            // Repare abaixo que depois que alterei essa linha o git já identificou que
+            // o arquivo sofreu alterações
+            Console.WriteLine("Linha alterada");
 
-            // Antes de tudo, verifique se você tem o git e github instalado, caso não tenha instale-os
-            // As abas ao lado são das duas ferramentas que mais 
-            // utilizamos no dia a dia (dependendo da empresa, projeto e 
-            // grupo que seu código fonte estará guardado)
-
-            // Você pode criar um repositório local com o git
-            // Como também você pode ter um repositório remoto (que é o github)
+            // Agora vamos enviar essas alterações para o nosso repositório remoto.
         }
     }
 }
